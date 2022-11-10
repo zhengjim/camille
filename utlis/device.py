1be9236d7b0d8d4369ba0e0e84df5c660dc35c87
Reborn <ren_xiaoyao@outlook.com>
2022-11-10 16:00:55 +0800

    fix: 修复偶尔 frida/adb 迷惑行为丢失连接的设备

--- utlis/device.py
+++ utlis/device.py
@@ -1,10 +1,33 @@
+import subprocess
+import time
+
 from utlis import print_msg
 from utlis.third_party_sdk import ThirdPartySdk
 import frida
 import pandas as pd
 
 
+def restart_adb(wait_time=1.5):
+    # 避免有时候 frida/adb 迷惑行为拿不到所有连接的设备
+    print("[*] 正在重启 ADB 服务")
+    restart_adb_shells = ["adb kill-server", "adb start-server"]
+    try:
+        for shell in restart_adb_shells:
+            shell_output = subprocess.getoutput(shell)
+            if shell_output == "":
+                continue
+            print(shell_output)
+        # 这里要睡眠至少 1.5 秒，不然可能 adb 服务还没完全开启，导致设备列表为空
+        time.sleep(wait_time)
+        # subprocess.getoutput("adb devices")
+        print("[*] 重启 ADB 服务成功")
+    except Exception as e:
+        print_msg(e)
+    print()
+
+
 def get_device_info():
+    restart_adb()
     devices = frida.enumerate_devices()
     # devices = list(filter(lambda d: d.name != "Local System" and d.name != "Local Socket",
     #                       frida.enumerate_devices()))
